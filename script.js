e2556f8a109dfce57d4b1843ab3a8aaebaf71b49
ABGer2025 <abgabg2024@hotmail.com>
2025-07-29 11:50:39 +0200

    Änderungen JavaScript Query Selektoren

--- script.js
+++ script.js
@@ -22,13 +22,20 @@ console.log("Anzahle der Elemente mit der Klasse content:", contentElements.leng
 
 for (let element of contentElements) {
     if (element.id === "content_second") {
-        element.innerHTML = "<p>Dieser Inhalt des zweiten Bereichs wurde mit getElementsByClassName aktualisiert.</p>"
+        element.innerHTML = "<p>Dieser Inhalt des zweiten Bereichs wurde mit getElementsByClassName aktualisiert.</p>";
         break;
     }
 }
 
 let messageDiv = document.getElementById("message");
 if (messageDiv) {
-    messageDiv.innerHTML = "<p>NAchrichten Div-Inhalt wurde mit getElementById aktualisiert.</p>";
-    break;
+    messageDiv.innerHTML = "<p>Nachrichten Div-Inhalt wurde mit getElementById aktualisiert.</p>";
 }
+
+let firstContentParagraph = document.querySelector("#content_first p");
+if (firstContentParagraph) {
+    firstContentParagraph.textContent = "Der erste Absatz des ersten Bereichs mit querySelector aktualisiert.";
+}
+
+let allContentDivs = document.querySelectorAll(".content");
+console.log("Alle Content-Divs vis querySelectorAll:", allContentDivs.length);
