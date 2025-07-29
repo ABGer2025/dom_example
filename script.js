cd350b71e1e808958561089bbc7f972c08038191
ABGer2025 <abgabg2024@hotmail.com>
2025-07-29 11:33:03 +0200

    Änderungen für JavaScript Demonstration

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("DOM-Script geladen.")
+
+let bodyElement = document.getElementsByTagName("body")[0];
+
+//console.log("Inhalt des Body-Elements: ", bodyElement.innerHTML);
+
+//bodyElement.innerHTML = "<p>Dieser Inhalt wurde von JavaScript ersetzt.</p>";
+
+let allElements = document.getElementsByTagName("*");
+
+console.log("Anzahl aller Elemente auf der Seite:", allElements.length);
+
+for (let element of allElements) {
+    if (element.id === "content_first") {
+        element.innerHTML = "<p>Dieser Inhalt wurde mit getElementsByTagName gefunden und aktualisiert.</p>";
+        break;
+    }
+}
+
+let contentElements = document.getElementsByClassName("content");
+console.log("Anzahle der Elemente mit der Klasse content:", contentElements.length);
+
+for (let element of contentElements) {
+    if (element.id === "content_second") {
+        element.innerHTML = "<p>Dieser Inhalt des zweiten Bereichs wurde mit getElementsByClassName aktualisiert.</p>"
+        break;
+    }
+}
+
+let messageDiv = document.getElementById("message");
+if (messageDiv) {
+    messageDiv.innerHTML = "<p>NAchrichten Div-Inhalt wurde mit getElementById aktualisiert.</p>";
+    break;
+}
